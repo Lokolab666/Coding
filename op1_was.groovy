26ca2aa3d80bad5eb2cc38c60da27d91632b5a19
Fandi <37313444+Lokolab666@users.noreply.github.com>
2026-02-10 13:40:29 -0500

    Refactor op1_was.groovy for improved structure

--- op1_was.groovy
+++ op1_was.groovy
@@ -1,47 +1,26 @@
-withCredentials([
-    usernamePassword(
-        credentialsId: GlobVars.accessWasco,
-        usernameVariable: 'USER_WAS',
-        passwordVariable: 'PASS_WAS'
-    )
-]) {
-    // Primero, sube el script Jython
-    sh 'scp get_server_info.jy svc.col.wasuat.service@10.171.183.6:/tmp/'
+variables:
+  RUN_AIKIDO_SCAN: "true"
+  AIKIDO_LOCAL_SCANNER_TOKEN: $AIKIDO_LOCAL_SCANNER_TOKEN
+  TRIVY_DB_REPOSITORY: "ghcr.io/aquasecurity/trivy-db"
 
-    // Luego, genera un wrapper shell en el remoto que reciba credenciales vía stdin o variable local
-    sh '''
-        ssh svc.col.wasuat.service@10.171.183.6 bash << 'EOF'
-            read -r USER_INPUT
-            read -r PASS_INPUT
-            /IBM/WebSphere/AppServer/bin/wsadmin.sh \
-                -username "$USER_INPUT" \
-                -password "$PASS_INPUT" \
-                -lang jython \
-                -profileName DmgrDIG \
-                -f /tmp/get_server_info.jy
-        EOF
-    ''' + " <<< \$'${USER_WAS}\n${PASS_WAS}'"
-}
+default:
+  timeout: 3h
+
+before_script:
+  - echo "🔍 Checking access to ghcr.io"
+  - curl -v https://ghcr.io 2>&1 | grep "HTTP/2 200" || (echo "❌ ghcr.io unreachable" && exit 1)
+
+include:
+  - project: 'CST_Aible_Hub/converged-planning/cp-cicd-tools'
+    ref: $AIKIDO_TEMPLATE_REF
+    file: '/templates/aikido-scan.yml'
+
+stages:
+  - code-check
+  - docker-image-build
+  - monitor
 
 
-------------------------------------
 
-withCredentials([
-    usernamePassword(
-        credentialsId: GlobVars.accessWasco,
-        usernameVariable: 'USER_WAS',
-        passwordVariable: 'PASS_WAS'
-    )
-]) {
-    def output = sh(
-        script: """
-            ${WSADMIN_PATH} \
-                -username "\$USER_WAS" \
-                -password "\$PASS_WAS" \
-                -lang jython \
-                -profileName DmgrDIG \
-                -f get_server_info.jy
-        """,
-        returnStdout: true
-    ).trim()
-}
+before_script:
+  - curl -v https://ghcr.io 2>&1 | grep "HTTP/2 200" || (echo "❌ ghcr.io unreachable" && exit 1)
